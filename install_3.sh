b8093e58e26f840876271fcd8c6f9592d4fd6335
Tommy SIN <64023236+TommySinPolyU@users.noreply.github.com>
2024-07-23 15:13:51 +0800

    Update Installation

--- install_3.sh
+++ install_3.sh
@@ -184,11 +184,11 @@ run_ollama_backend(){
 
 download_required_models(){
     #ollama pull shaw/dmeta-embedding-zh
-    ollama pull rjmalagon/gte-qwen2-7b-instruct-embed-f16
+    #ollama pull rjmalagon/gte-qwen2-7b-instruct-embed-f16
     ollama pull quentinz/bge-large-zh-v1.5
     #ollama pull pokmon/llama3-8b-chinese
     #ollama pull qwen2
-	ollama pull qwen:7b
+	ollama pull qwen:1.8b
 }
 
 
